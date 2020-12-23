2b268c3096e0405771f075b2fd055b8989714cc1
yujin779 <51077021+yujin779@users.noreply.github.com>
2020-12-24 02:35:47 +0900

    テクスチャを配列に変更

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import * as THREE from "three";
 import { /*Button, Image, StyleSheet, */ Text, View } from "react-native";
 import { Canvas, useFrame /*, useThree*/ } from "react-three-fiber";
 import styles from "./styles";
-import texture from "./assets/img/texture_new2_water_px.png";
+import texture1 from "./assets/img/texture_floor3_normal_light_px.png";
+import texture2 from "./assets/img/texture_floor_octogons.png";
+import texture3 from "./assets/img/texture_floortile_8.png";
+import texture4 from "./assets/img/texture_new2_water_px.png";
+import texture5 from "./assets/img/texture_new_bluerocks.png";
+import texture6 from "./assets/img/texture_rocks_moss_px.png";
 
-const three_texture = new THREE.TextureLoader().load(texture);
+const loader = new THREE.TextureLoader();
+const textures = [
+  loader.load(texture1),
+  loader.load(texture2),
+  loader.load(texture3),
+  loader.load(texture4),
+  loader.load(texture5),
+  loader.load(texture6)
+];
+console.log(textures);
 // テクスチャを並べて表示
 // three_texture.wrapS = THREE.RepeatWrapping;
 // three_texture.wrapT = THREE.RepeatWrapping;
 // //テクスチャの倍率、1,1=全面に貼り付け
-three_texture.repeat.set(1, 1);
+// three_texture.repeat.set(1, 1);
 
 /*
  * 2.boxのmeshを作成して返す
@@ -28,7 +42,7 @@ const Box = (props) => {
       {/* ジオメトリ(形)とマテリアル(色など)をメッシュの中にいれる */}
       <boxBufferGeometry args={[1, 1, 1]} />
       {/* <meshStandardMaterial color={"orange"} /> */}
-      <meshBasicMaterial map={three_texture} />
+      <meshBasicMaterial map={textures[0]} />
     </mesh>
   );
 };
